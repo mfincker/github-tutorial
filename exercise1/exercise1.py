1bb9b631cb57f9eb5740f1ea619fae9bea9bc3f6
“Maeva <maeva.fincker@gmail.com>
2019-12-23 16:28:56 -0500

    exercise1 - adding print_art function

--- exercise1/exercise1.py
+++ exercise1/exercise1.py
@@ -2,3 +2,17 @@
 
 def print_message(message):
 	print (message)
+
+
+def print_art():
+	art = """    |\\__/,|   (`
+  _.|o o  |_   ) )
+ -(((---(((--------
+"""
+
+	print (art)
+
+
+if __name__ == '__main__':
+	print_art()
+
